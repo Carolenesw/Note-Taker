3e33f1c3a86fdb93928b6df6dc1dd2777178cf90
Carolene Wilson <carolenesw@gmail.com>
2020-07-10 21:45:51 -0400

    use express app to handle data parsing and join files

--- server.js
+++ server.js
@@ -2,41 +2,30 @@
 const express = require("express");
 const path = require("path");
 
-// use body parser to gives access to properties and variables
-const bodyParser = require("body-parser");
-// app.use(bodyParser.urlencoded({extended: true}));
-
 // inform node that we are creating an express server 
 const app = express();
+
+// use Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/public/assets")));
+app.use(express.json());
 
 // Sets an initial port to use to listen
 const PORT = process.env.PORT || 8000;
 
-
+// set get request for index index file
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/public/index.html")
+  res.sendFile(path.join(__dirname, "/public/index.html"));
     // res.send("Node Server App!")
-    // console.log(__dirname);
+    console.log(__dirname);
 });
 
 // set get request for notes index file
 app.get("/notes", function (req, res){
-  res.sendFile(__dirname + "/public/notes.html")
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
   
   });
-// bmi calculator post resquest
-// app.post("/bmicalculator", function (req, res){
-
-//   console.log("BMI request:", req.body);
- 
-// //     // parseFloat gives a decimal value return
-//   var weight = parseFloat(req.body.weight);
-//   var height = parseFloat(req.body.height);
-
-//   var bmiResult = weight / (height * height);
-
-//   res.send("Your BMI results is: " + bmiResult);
-//   });
+  
 // start server
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
