7fab9de318033bb7c1bfe01ef023333961151db3
Carolene Wilson <carolenesw@gmail.com>
2020-07-10 21:02:37 -0400

    get request for index.js file

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 // create dependency variables 
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // use body parser to gives access to properties and variables
 const bodyParser = require("body-parser");
 // app.use(bodyParser.urlencoded({extended: true}));
 
 // inform node that we are creating an express server 
-var app = express();
+const app = express();
 
 // Sets an initial port to use to listen
-var PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/public/index.html")
+    // res.send("Node Server App!")
     // console.log(__dirname);
 });
 
