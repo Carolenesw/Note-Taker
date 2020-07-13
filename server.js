90aedbfc8fa842d0cdd6cb0966fceb6faf47b742
Carolene Wilson <carolenesw@gmail.com>
2020-07-12 23:26:19 -0400

    add error page

--- server.js
+++ server.js
@@ -13,28 +13,31 @@ app.use(express.json());
 // sets an initial port to use to listen
 const PORT = process.env.PORT || 8000;
 
-// set get request for root index html file
+// set get request for root index html file/send response
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
     // res.send("Node Server App!")
     console.log(__dirname);
 });
 
-// set get request for notes html file
+// add error page send response
+app.get("*", (req, res)=> {
+  res.json("404, Page not found")
+})
+// set get request for notes html file/send response
 app.get("/notes", function (req, res){
   res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
   
-// set çAPI Routes
+// set API Routes
 // app.get("/api/notes", (req, res) => {
 //   return res.send(db);
 // });
-
 app.get('/api/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/db.json'));
 });
 
 // start server
-app.listen(PORT, function() {
+app.listen(PORT, () => {
     console.log("App listening on PORT: " + PORT);
   });
