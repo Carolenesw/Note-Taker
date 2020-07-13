588e80a3c8897d2944e588b8a46da26f6dcc8649
Carolene Wilson <carolenesw@gmail.com>
2020-07-12 22:13:31 -0400

    add api route

--- server.js
+++ server.js
@@ -10,22 +10,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "/public/assets")));
 app.use(express.json());
 
-// Sets an initial port to use to listen
+// sets an initial port to use to listen
 const PORT = process.env.PORT || 8000;
 
-// set get request for index index file
+// set get request for root index html file
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
     // res.send("Node Server App!")
     console.log(__dirname);
 });
 
-// set get request for notes index file
+// set get request for notes html file
 app.get("/notes", function (req, res){
   res.sendFile(path.join(__dirname, "/public/notes.html"));
-  
   });
   
+// set çAPI Routes
+// app.get("/api/notes", (req, res) => {
+//   return res.send(db);
+// });
+
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/db.json'));
+});
+
 // start server
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
