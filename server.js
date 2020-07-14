33b41481a96957e769f56c882fb75e7c9c59e467
Carolene Wilson <carolenesw@gmail.com>
2020-07-14 13:36:19 -0400

    add delete note function

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-// use performance to generate random numbers
-const performance = require("performance");
+
 // inform node that we are creating an express server 
 const app = express();
 
@@ -15,21 +14,20 @@ app.use(express.json());
 // sets an initial port to use to listen
 const PORT = process.env.PORT || 8000;
 
-
 //reading the db.json and use ternary operator  to parse or push data to empty array
 let dBase = fs.readFileSync("./db/db.json","utf-8");
 console.log("database:", dBase)
 dBase ? dBase = JSON.parse(dBase) : dBase = [];
 
 // set get request for root index html file/send response
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
     // res.send("Node Server App!")
     console.log("notes:", __dirname);
 });
 
 // set get request for notes html file/send response
-app.get("/notes", function (req, res){
+app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
 
@@ -38,14 +36,10 @@ app.get('/api/notes', (req, res) => {
   return res.send(dBase);
 })
 
-// app.get("/api/notes", function(req, res) {
-//   return res.json(notes);
-// });
-
 // post new notes/receieved and process data
 app.post("/api/notes", (req, res) => {
   let body = req.body;
-  let uniqueId = {"id":Math.round(Math.random()* 99)};
+  let uniqueId = {"id":Math.round(Math.random()* 999)};
   
   console.log("id:", uniqueId)
   console.log("request:", body)
@@ -57,6 +51,17 @@ app.post("/api/notes", (req, res) => {
   console.log("post data:", dBase)
 });
 
+// delete notes/receieved and process data
+app.delete("/api/notes/:id", (req,res) => {
+  const noteId = dBase.find(note => note.id === parseInt(req.params.id));
+  const index = dBase.indexOf(noteId);
+  if(!noteId) 
+  return res.status(404).send("Note not available!");
+  
+  dBase.splice(index,1);
+  fs.writeFileSync("./db/db.json", JSON.stringify(dBase), "utf-8");
+  return res.json(true);
+});
 
 
 // add error page send response
